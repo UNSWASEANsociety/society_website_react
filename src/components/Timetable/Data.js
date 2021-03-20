8ec57ee9a45e050239528958cc8970cf95d2ae17
Tymotex <timzhang3@gmail.com>
2021-03-20 23:29:14 +1100

    Added sample data

---
+++ src/components/Timetable/Data.js
@@ -0,0 +1,122 @@
+import { ImageAspectRatioSharp } from '@material-ui/icons';
+import React from 'react';
+
+const day1Events = [
+    {
+        start: "11:00",
+        end: "11:15 am",
+        agenda: (
+        <>
+        Introductions
+        </>
+        ),
+        description: (
+            <>
+                Introductions will consist of outlining the Cultural immersion event, expectations and what we hope everyone will get out of the program
+            </>
+        )
+    },
+    {
+        start: "11:15",
+        end: "12:15 pm",
+        agenda: (
+        <>
+        Breakout Activity
+        </>
+        ),
+        description: (
+            <>
+                11:15 - 11:30 - Nadya Wang
+                11:30 - 11:45 - bingo
+                11:45 - 12:15pm - breakout activity
+                Congak
+                5 Stones
+                Fish Weaving
+                Colour in Southeast Asia Worksheet
+                SEA Musical Chairs
+            </>
+        )
+    },
+    {
+        start: "12:15",
+        end: "1:00 pm",
+        agenda: (
+        <>
+        Lunch 
+        </>
+        ),
+        description: (
+            <>
+                Lunch and the cultural significance of the food
+
+                Main 3 choices: Pad Thai, Chilli Basil & Fried Rice (vegetarian)
+
+                Dessert: Fried banana & Coconut custard
+            </>
+        )
+    },
+    {
+        start: "1:00",
+        end: "2:00 pm",
+        agenda: (
+        <>
+        The History & Future of SEA 
+        Professor Adrian Vickers (40 mins lecture & 20 mins Q&A)
+        Discussion Activity 
+        </>
+        ),
+        description: (
+            <>
+                External speakers will be invited to discuss the history and future of SEA. Some topics discussed will be diversity, marginalisation and opportunity.
+            </>
+        )
+    },
+    {
+        start: "2:00",
+        end: "3:00 pm",
+        agenda: (
+            <>
+                Discussion Activity 
+            </>
+        ),
+        description: (
+            <>
+                The Discussion Activity will provide students with the opportunity to discuss diversity, marginalisation and opportunity. 
+            </>
+        )
+    },
+    {
+        start: "3:00",
+        end: "3:15 pm",
+        agenda: (
+            <>
+                Conclusions 
+            </>
+        ),
+        description: (
+            <>
+                Reflection and summary of Day 1 
+            </>
+        )
+    }
+];
+
+export default {
+    day1Events: day1Events
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
